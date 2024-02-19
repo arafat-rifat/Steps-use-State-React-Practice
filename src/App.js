d8d6ba6f8bba6781fe59d76cc4a407044c6d7470
arafat-rifat <arafat15-12603@diu.edu.bd>
2024-02-20 04:38:43 +0600

    What is State In React

--- src/App.js
+++ src/App.js
@@ -8,25 +8,51 @@ const messages = [
 
 
 function App() {
+  
+  let step = 1;
+
+ const handlePrevious = function(){
+  step = step + 1;
+ }
+
+ const handleNext = function(){
+ step = step - 1;
+ }
+
+
   return (
    <div className="steps">
    
    {/* For Steps Of Number */}
   <div className="numbers">
-    <div className="active">1</div>
-    <div>2</div>
-    <div>3</div>
+      <div className={`${step >= 1 ? "active" : ""}`}>
+      1
+      </div>
+
+      <div className={`${step >= 2 ? "active" : ""}`}>
+      2
+      </div>
+
+       <div className={`${step >= 3 ? "active" : ""}`}>
+      3
+       </div>
   </div>
 
   {/* For text View Part */}
-  <p className="message">Hello</p>
+  <p className="message">Step {step}: {messages[step - 1]}</p>
 
 
   {/* For Button */}
    
    <div className="buttons">
-     <button style={{backgroundColor:'#7950f2', color:'#fff'}}>Previous</button>
-     <button style={{backgroundColor:'#7950f2', color:'#fff'}}>Next</button>
+     <button style={{backgroundColor:'#7950f2', color:'#fff'}}  onClick={handlePrevious}>Previous</button>
+
+     
+     <button style={{backgroundColor:'#7950f2', color:'#fff'}}
+      onClick={handleNext}
+      >
+        Next
+        </button>
    </div>
 
    </div>
